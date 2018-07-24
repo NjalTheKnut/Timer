3db7f20fe6743ee0e6cf92709adc87d948b4e1da
Neal R <nealjrobison@gmail.com>
2018-07-24 19:54:11 -0400

    Bug Fixes and Enhancements

--- TimerObject.py
+++ TimerObject.py
@@ -5,7 +5,7 @@ class TimerObject:
     interval = 0
     balance = 0
     
-    def __init__(self, name, accum=0, interval=15, balance=0):
+    def __init__(self, name, accum=0, interval=5, balance=0):
         self._name = name
         self._accum = accum
         self._interval = interval
@@ -39,38 +39,46 @@ class TimerObject:
     def interval(self, interval):
         self._interval = interval
     
+    def print_data(self, name, accum):
+        msg = name + ': {accum}'.format(accum = accum)
+        print(msg)
+
     def upCount(self):
         name = self.name
         accum = self.accum
         interval = self.interval
         temp = 0
         print('counting up on: ' + name)
-        msg = name+': {accum}'.format(accum = accum)
-        print(msg)
-        while temp < interval:
-            time.sleep(1)
-            temp += 1
-            accum += 1
-            msg = name+': {accum}'.format(accum = accum)
-            print(msg)
-        self.accum = accum
+        self.print_data(name, accum)
+        stop = false
+        while stop == false:
+            if accum >= limit:
+                stop = true
+            elif temp < interval:
+                time.sleep(1)
+                temp += 1
+                accum += 1
+                self.print_data(name, accum)
+            self.accum = accum
     
     def downCount(self):
         name = self.name
         accum = self.accum
         interval = self.interval
         print('counting down on: ' + name)
-        msg = name+': {accum}'.format(accum = accum)
-        print(msg)
+        self.print_data(name, accum)
         while accum > 0:
             time.sleep(1)
             accum -= 1
-            msg = name+': {accum}'.format(accum = accum)
-            print(msg)
+            self.print_data(name, accum)
         self.accum = accum
+
 timer = TimerObject('test')
 str = 'name: {x}'.format(x=timer.name)
 str = 'accum: {x}'.format(x=timer.accum)
 str = 'interval: {x}'.format(x=timer.interval)
+timer.upCount()
+timer.downCount()
+timer.upCount()
 timer.upCount()
 timer.downCount()
