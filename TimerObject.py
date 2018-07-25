e1f9385608ab9c62170bef6d29da78e4c935c610
Neal R <nealjrobison@gmail.com>
2018-07-25 11:41:42 -0400

    Edits to Interval Functionality

--- TimerObject.py
+++ TimerObject.py
@@ -1,32 +1,20 @@
 import time
 class TimerObject:
-    class_name = ""
-    accum = 0
-    interval = 0
-    balance = 0
-    
-    def __init__(self, name, accum=0, interval=5, balance=0):
+    def __init__(self, name, accum=0, interval=5):
         self._name = name
         self._accum = accum
         self._interval = interval
-        self._balance = balance
     
     @property
     def name(self):
         return self._name
-    
     @property
     def accum(self):
         return self._accum
-    
     @property
     def interval(self):
         return self._interval
-    
-    @property
-    def balance(self, balance):
-        return self._balance
-    
+
     @name.setter
     def name(self, name):
         self._name = name
@@ -39,46 +27,51 @@ class TimerObject:
     def interval(self, interval):
         self._interval = interval
     
-    def print_data(self, name, accum):
+    def print_accum(self, name, accum):
         msg = name + ': {accum}'.format(accum = accum)
         print(msg)
-
-    def upCount(self):
-        name = self.name
-        accum = self.accum
-        interval = self.interval
-        temp = 0
-        print('counting up on: ' + name)
-        self.print_data(name, accum)
-        stop = false
-        while stop == false:
-            if accum >= limit:
-                stop = true
-            elif temp < interval:
-                time.sleep(1)
-                temp += 1
-                accum += 1
-                self.print_data(name, accum)
-            self.accum = accum
     
-    def downCount(self):
+    def print_data(self):
+        msg = 'name: {x}'.format(x=self.name)
+        print(msg)
+        msg = 'accum: {x}'.format(x=self.accum)
+        print(msg)
+        msg = 'interval: {x}'.format(x=self.interval)
+        print(msg)
+    
+    def upCount(self, limit):
+        spam = limit + 1
         name = self.name
         accum = self.accum
         interval = self.interval
+        eggs = 0
+        print('counting up on: ' + name)
+        for interval in range(spam):
+            while eggs < interval:
+                self.print_accum(name, accum)
+                time.sleep(1)
+                eggs += 1
+                accum += 1
+                self.accum = accum
+        self.print_accum(name, accum)
+    
+    def downCount(self, limit):
+        spam = limit + 1
+        name = self.name
+        accum = self.accum
+        interval = self.interval
+        eggs = 0
         print('counting down on: ' + name)
-        self.print_data(name, accum)
-        while accum > 0:
-            time.sleep(1)
-            accum -= 1
-            self.print_data(name, accum)
-        self.accum = accum
+        for interval in range(spam):
+            while eggs < interval:
+                self.print_accum(name, accum)
+                time.sleep(1)
+                eggs += 1
+                accum -= 1
+            self.accum = accum
+        self.print_accum(name, accum)
 
 timer = TimerObject('test')
-str = 'name: {x}'.format(x=timer.name)
-str = 'accum: {x}'.format(x=timer.accum)
-str = 'interval: {x}'.format(x=timer.interval)
-timer.upCount()
-timer.downCount()
-timer.upCount()
-timer.upCount()
-timer.downCount()
+timer.upCount(10)
+timer.downCount(5)
+timer.print_data()
